db2cbfb49c680972d030052af6fbf76f699d573f
Nirvana Mauricio Braga <nirvanamb2007@gmail.com>
2026-05-31 19:45:13 -0300

    Add Funcionario class with attributes and methods

--- Funcionario.java
+++ Funcionario.java
@@ -1,1 +1,44 @@
+public class Funcionario {
+    // Atributos privados
+    private String nome;
+    private String cargo;
+    private double salario;
+    private int matricula;
 
+    // Construtor
+    public Funcionario(String nome, String cargo, double salario, int matricula) {
+        this.nome = nome;
+        this.cargo = cargo;
+        this.salario = salario;
+        this.matricula = matricula;
+    }
+
+    // Método para aplicar o aumento salarial baseado no percentual
+    public void aplicarAumento(double percentual) {
+        if (percentual > 0) {
+            this.salario += this.salario * (percentual / 100);
+        }
+    }
+
+    // Método para exibir os dados do funcionário
+    public void exibirDados() {
+        System.out.println("- Matrícula: " + matricula + " | Nome: " + nome + " | Cargo: " + cargo + " | Salário: R$ " + salario);
+    }
+
+    // Getters para a classe Main conseguir consultar as informações do objeto
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+}
