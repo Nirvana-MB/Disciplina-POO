c9b92c31cce52db3c8442a94c0a7da3a9b1db3e5
Nirvana Mauricio Braga <nirvanamb2007@gmail.com>
2026-05-31 19:10:49 -0300

    Initialize employee management system in Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
 
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    Funcionario[] listaFuncionarios = new Funcionario[10]; // Limite de funcionários
+    int contadorFuncionarios = 0; // Começa com 0 funcionários cadastrados por padrão
+
+    int opcao;
+
+    do {
+            System.out.println("- Sistema de consulta para funcionários");
+            System.out.println("1. Cadastrar funcionário");
+            System.out.println("2. Listar funcionários");
+            System.out.println("3. Buscar funcionário");
+            System.out.println("4. Aplicar aumento salarial");
+            System.out.println("5. Mostrar folha salarial");
+            System.out.println("6. Sair");
+            System.out.print("Digite um número: ");
+            opcao = scanner.nextInt();
+            scanner.nextLine();
+}
