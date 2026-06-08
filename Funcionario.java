422e40b61dcfeff79dd845448ceafe5e7a88a945
Mc._.Rancan <mariaclara23crp@gmail.com>
2026-06-08 12:56:25 -0300

    Update Funcionario.java

--- Funcionario.java
+++ Funcionario.java
@@ -1,11 +1,11 @@
 public class Funcionario {
-// 
+// atributos
     private String nome;
     private String cargo;
     private double salario;
     private int matricula;
 
-//
+// construtor
     public Funcionario(String nome, String cargo, double salario, int matricula) {
         this.nome = nome;
         this.cargo = cargo;
@@ -13,19 +13,19 @@ public class Funcionario {
         this.matricula = matricula;
     }
 
-// 
+// metodos de aplicar aumento
     public void aplicarAumento(double percentual) {
         if (percentual > 0) {
             this.salario += this.salario * (percentual / 100);
         }
     }
 
-// 
+// metodos de mostrar dados
     public void exibirDados() {
         System.out.println("- Matrícula: " + matricula + " | Nome: " + nome + " | Cargo: " + cargo + " | Salário: R$ " + salario);
     }
 
-// 
+// getters
     public String getNome() {
         return nome;
     }
